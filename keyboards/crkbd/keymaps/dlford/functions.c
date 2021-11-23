4d234f77bcf5c7d28176e9e4250cc3b6452dd9e9
dlford <dan-github2@dlford.co>
2021-11-22 20:19:43 -0700

    start migrating away from json keymap

--- keyboards/crkbd/keymaps/dlford/functions.c
+++ keyboards/crkbd/keymaps/dlford/functions.c
@@ -39,6 +39,25 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define _M8 KC_F21
 #define _M9 KC_F22
 
+/*
+// Quantum keys
+#define CSA_Q MEH_T(KC_Q)
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+  [_QWERTY] = LAYOUT_split_3x5_3(
+  //|--------------------------------------------|                    |--------------------------------------------|
+      KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,
+  //|--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------|
+      KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L,   KC_SCLN,
+  //|--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------|
+      KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT,   KC_SLSH,
+  //|--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------|
+                                 KC_LGUI,   MO(1),  KC_SPC,    KC_ENT,   MO(2), KC_RALT
+  //                           |--------+--------+--------|  |--------+--------+--------|
+  ),
+};
+*/
+
 static uint16_t default_animation = RGB_MATRIX_CYCLE_SPIRAL;
 static int default_speed = 50;
 static uint16_t secondary_animation = RGB_MATRIX_HUE_WAVE;
