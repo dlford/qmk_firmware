e507044ccc48b94d023442967f835677288b301a
dlford <dan-github2@dlford.co>
2023-04-03 20:54:06 -0700

    userspace WIP

--- keyboards/crkbd/keymaps/dlford/keymap.c
+++ keyboards/crkbd/keymaps/dlford/keymap.c
@@ -12,9 +12,6 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#define ALT_TAB_USER_ENABLE
-#define ALL_MATRIX_ANIMATIONS_USER_ENABLE
-
 #include QMK_KEYBOARD_H
 #include "dlford.h"
 
--- users/dlford/rules.mk
+++ users/dlford/rules.mk
@@ -2,9 +2,8 @@ SRC += tap_rules.c
 SRC += pwgen_user.c
 SRC += mouse_jiggler_user.c
 SRC += sarcasm_mode.c
-ifdef ALT_TAB_USER_ENABLE
 SRC += alt_tab_user.c
-endif
+SRC += eeprom_user.c
 ifdef OLED_ENABLE
 	SRC += oled_user.c
 endif
@@ -14,7 +13,4 @@ endif
 ifdef RGB_MATRIX_ENABLE
 	SRC += rgb_timeout_user.c
 	SRC += rgb_matrix_user.c
-	ifdef ALL_MATRIX_ANIMATIONS_USER_ENABLE
-		SRC += eeprom_user.c
-	endif
 endif
