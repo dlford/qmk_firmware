9674821e561794f25d445d899ad9ba45eb1b5a98
dlford <dan-github2@dlford.co>
2023-04-04 07:59:00 -0700

    userspace WIP

--- users/dlford/process_record_user.c
+++ users/dlford/process_record_user.c
@@ -24,40 +24,20 @@ __attribute__((weak)) bool process_record_keymap(uint16_t keycode, keyrecord_t *
     return true;
 }
 
+bool custom_keycodes_result       = true;
+bool process_record_keymap_result = true;
+
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 #ifdef RGB_MATRIX_ENABLE
     process_record_rgb_timeout(record);
 #endif
     process_record_sarcasm_mode(record);
+    custom_keycodes_result       = process_record_custom_keycodes_user(keycode, record);
+    process_record_keymap_result = process_record_keymap(keycode, record);
 
-    switch (keycode) {
-        case M_ALT_TAB:
-            start_alt_tab(record);
-            break;
-        case M_RGB_SPD:
-            if (record->event.pressed) {
-                if (keyboard_report->mods & MOD_BIT(KC_LSFT) || keyboard_report->mods & MOD_BIT(KC_RSFT)) {
-                    user_config.rgb_speed = (user_config.rgb_speed - 10) % 256;
-                    rgb_matrix_set_speed_noeeprom(user_config.rgb_speed);
-                    write_user_config();
-                } else {
-                    user_config.rgb_speed = (user_config.rgb_speed + 10) % 256;
-                    rgb_matrix_set_speed_noeeprom(user_config.rgb_speed);
-                    write_user_config();
-                }
-            }
-            break;
-        case M_RST_RGB:
-            if (record->event.pressed) {
-                layer_off(_MOUSE);
-                rgb_matrix_mode(RGB_MATRIX_CYCLE_SPIRAL);
-                rgb_matrix_sethsv(HSV_ORANGE);
-                user_config.rgb_speed = 50;
-                rgb_matrix_set_speed_noeeprom(user_config.rgb_speed);
-                write_user_config();
-            }
-            break;
+    if (custom_keycodes_result && process_record_keymap_result) {
+        return true;
+    } else {
+        return false;
     }
-
-    return process_record_keymap(keycode, record);
 }
