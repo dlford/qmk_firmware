550cfbe85798ba46eed482b0068694c36da65534
dlford <dan-github2@dlford.co>
2023-04-09 22:15:04 -0700

    disable rgb idle timeout if idle animation enabled

--- users/dlford/rgb_timeout_user.c
+++ users/dlford/rgb_timeout_user.c
@@ -16,6 +16,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #include QMK_KEYBOARD_H
+#include "rgb_timeout_user.h"
+#include "eeprom_user.h"
 
 int      RGB_CUSTOM_TIMEOUT          = 5;
 uint16_t rgb_timeout_user_idle_timer = 0;
@@ -23,7 +25,7 @@ uint8_t  halfmin_counter             = 0;
 bool     is_rgb_timeout_user_on      = true;
 
 void matrix_scan_rgb_timeout(void) {
-    if (is_keyboard_master()) {
+    if (is_keyboard_master() && !user_config.is_rgb_idle_enabled) {
         if (rgb_timeout_user_idle_timer == 0) rgb_timeout_user_idle_timer = timer_read();
 
         if (is_rgb_timeout_user_on && timer_elapsed(rgb_timeout_user_idle_timer) > 30000) {
@@ -40,7 +42,7 @@ void matrix_scan_rgb_timeout(void) {
 }
 
 void process_record_rgb_timeout(keyrecord_t *record) {
-    if (is_keyboard_master()) {
+    if (is_keyboard_master() && !user_config.is_rgb_idle_enabled) {
         if (record->event.pressed) {
             if (is_rgb_timeout_user_on == false) {
                 rgb_matrix_enable_noeeprom();
