55b45390c2a2d1bc1360b5c692b42894030cdd82
dlford <dan-github2@dlford.co>
2023-04-21 13:14:57 -0700

    playing with HID RAW

--- users/dlford/hid_raw_user.c
+++ users/dlford/hid_raw_user.c
@@ -17,27 +17,53 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include QMK_KEYBOARD_H
 #include "raw_hid.h"
+#ifdef CONSOLE_ENABLE
+#    include "print.h"
+#endif
 
-bool is_notification_on  = false;
-bool is_hidraw_connected = false;
+bool is_hidraw_user_notification_on = false;
+bool is_hidraw_user_connected       = false;
+char hidraw_user_string_value[32];
+int  hidraw_user_cpu_usage;
+
+enum hid_raw_user_indices {
+    NOTIFICATION = 1,
+    CPU_USAGE,
+    STRING_VALUE,
+};
 
 void raw_hid_receive(uint8_t *data, uint8_t length) {
-    is_hidraw_connected = true;
+    is_hidraw_user_connected = true;
+#ifdef CONSOLE_ENABLE
+    debug_enable = true;
+#endif
 
-    // uint8_t send_data[32] = {1};
-    // raw_hid_send(send_data, sizeof(send_data));
-    raw_hid_send(data, length);
-
-    /*
-    char *str = (char *)data;
-    if (strcmp(str, "toggle_notification") == 0) {
-        is_notification_on = !is_notification_on;
+    uint8_t res[32] = {data[0], 1};
+    switch (data[0]) {
+        case NOTIFICATION:
+            is_hidraw_user_notification_on = data[1] == 1;
+            break;
+        case CPU_USAGE:
+            hidraw_user_cpu_usage = data[1];
+#ifdef CONSOLE_ENABLE
+            uprintf("CPU usage: %d\n", hidraw_user_cpu_usage);
+#endif
+            /*
+            char snum[5];
+            itoa(hidraw_user_cpu_usage, snum, 10);
+            send_string(snum);
+            */
+            break;
+        case STRING_VALUE:
+            memcpy(hidraw_user_string_value, (char *)&data[1], sizeof(hidraw_user_string_value));
+#ifdef CONSOLE_ENABLE
+            uprintf("String value: %s\n", hidraw_user_string_value);
+#endif
+            // send_string(hidraw_user_string_value);
+            break;
+        default:
+            res[1] = 0;
     }
 
-    if (is_notification_on) {
-        send_string("notified!");
-    } else {
-        send_string("not notified!");
-    }
-    */
+    raw_hid_send(res, sizeof(res));
 }
